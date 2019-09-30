a7b5070a99065e8b26680c441986d3ac9002223d
Shiyu Li <timlee0212@outlook.com>
2019-09-30 11:13:08 -0400

    Add LR Compare

--- logs/plotlc-cmp.py
+++ logs/plotlc-cmp.py
@@ -2,29 +2,34 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-a1 = pd.read_csv("training_lr0.1.csv")
-a2 = pd.read_csv("training_lr0.1_bn.csv")
+bn01 = pd.read_csv("training_lr0.1_bn.csv")
+bn005 = pd.read_csv("training_lr0.05_bn.csv")
+bn001 = pd.read_csv("training_lr0.01_bn.csv")
 
 sns.set_style("whitegrid")
 
-sns.set_palette(sns.color_palette("coolwarm", 4))
+sns.set_palette(sns.color_palette("Paired"))
 fig = plt.figure()
-sns.lineplot(x='epochs', y=" train_acc", ci=None, data=a1)
-sns.lineplot(x='epochs', y=" val_acc", ci=None, data=a1)
-sns.lineplot(x='epochs', y=" train_acc", ci=None, data=a2)
-sns.lineplot(x='epochs', y=" val_acc", ci=None, data=a2)
-plt.legend(["Train Acc","Val Acc", "Train Acc w\BN", "Val Acc w\BN"])
+sns.lineplot(x='epochs', y=" train_acc", ci=None, data=bn01)
+sns.lineplot(x='epochs', y=" val_acc", ci=None, data=bn01)
+sns.lineplot(x='epochs', y=" train_acc", ci=None, data=bn005)
+sns.lineplot(x='epochs', y=" val_acc", ci=None, data=bn005)
+sns.lineplot(x='epochs', y=" train_acc", ci=None, data=bn001)
+sns.lineplot(x='epochs', y=" val_acc", ci=None, data=bn001)
+plt.legend(["Train 0.1", "Val 0.1", "Train 0.05", "Val 0.05", "Train 0.01", "Val 0.01"])
 plt.xlabel("Epochs")
 plt.ylabel("Accuracy")
 
 plt.show()
 
 fig = plt.figure()
-sns.lineplot(x='epochs', y=" train_loss", ci=None, data=a1)
-sns.lineplot(x='epochs', y=" val_loss", ci=None, data=a1)
-sns.lineplot(x='epochs', y=" train_loss", ci=None, data=a2)
-sns.lineplot(x='epochs', y=" val_loss", ci=None, data=a2)
-plt.legend(["Train Loss","Val Loss", "Train Loss w\BN", "Val Loss w\BN"])
+sns.lineplot(x='epochs', y=" train_loss", ci=None, data=bn01)
+sns.lineplot(x='epochs', y=" val_loss", ci=None, data=bn01)
+sns.lineplot(x='epochs', y=" train_loss", ci=None, data=bn005)
+sns.lineplot(x='epochs', y=" val_loss", ci=None, data=bn005)
+sns.lineplot(x='epochs', y=" train_loss", ci=None, data=bn001)
+sns.lineplot(x='epochs', y=" val_loss", ci=None, data=bn001)
+plt.legend(["Train 0.1", "Val 0.1", "Train 0.05", "Val 0.05", "Train 0.01", "Val 0.01"])
 plt.xlabel("Epochs")
 plt.ylabel("Loss")
 
